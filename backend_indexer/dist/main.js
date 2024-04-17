99d0f5296b5af4cde5ca2b8d379ff96ec9660d5f
panyongxu1002 <panyx1002@gmail.com>
2024-04-18 00:49:30 +0800

    update swagger config

--- backend_indexer/dist/main.js
+++ backend_indexer/dist/main.js
@@ -1,17 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const logger = new common_1.Logger();
     const options = new swagger_1.DocumentBuilder()
         .setTitle('EthStorage Browser Indexer Api')
         .setVersion('1.0')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, options);
-    swagger_1.SwaggerModule.setup('apiDoc', app, document);
-    await app.listen(3001);
+    swagger_1.SwaggerModule.setup('api-docs', app, document, {
+        customSiteTitle: 'Backend Generator',
+        customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+        customJs: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+        ],
+        customCssUrl: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+        ],
+    });
+    await app.listen(3001, () => {
+        logger.debug(`Server listen on:: http://localhost:3001`);
+        logger.debug(`swagger文档: http://localhost:3001/api-docs`);
+    });
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- backend_indexer/src/main.ts
+++ backend_indexer/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
   const options = new DocumentBuilder()
     .setTitle('EthStorage Browser Indexer Api')
     // .setDescription('eth_storage backend_indexer')
@@ -12,8 +13,24 @@ async function bootstrap() {
     // .addTag('Api/V1')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('apiDoc', app, document);
-  await app.listen(3001);
+  SwaggerModule.setup('api-docs', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
+
+  await app.listen(3001, () => {
+    logger.debug(`Server listen on:: http://localhost:3001`);
+    logger.debug(`swagger文档: http://localhost:3001/api-docs`);
+  });
 }
 
 bootstrap();
